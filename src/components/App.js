37751c1476ea6ca921e753e0bb46323c824577d7
Veves <vevespm@gmail.com>
2021-10-04 13:21:40 +0200

    basic structure and firts html

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <header>
+        <img alt="logo de Rick y Morty" src="./logo.jpg"/>
+    </header>
+    <main>
+        <form class="filter">
+            <input type="text" class="search"/>
+        </form>
+        <section class="cards">
+            <div class="card">
+
+            </div>
+            <div class="card">
+
+            </div>
+            <div class="card">
+
+            </div>
+            <div class="card">
+
+            </div>
+            <div class="card">
+
+            </div>
+            <div class="card">
+
+            </div>
+            <div class="card">
+
+            </div>
+        </section>
+    </main>
+    
+    </div>
+  );
+}
+
+export default App;
